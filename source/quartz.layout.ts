1e2c34dbc0b498c56a397d73eec7bbf824ee8158
Mike Lines <lines0812@gmail.com>
2025-09-04 08:39:22 +1200

    remove discord link, edit layout

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/mikantlin/journey-wreathed-in-ash",
     },
   }),
 }
@@ -30,7 +29,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Content(),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
